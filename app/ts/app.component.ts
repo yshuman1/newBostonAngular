3e37413badd9be4397093c5d9016213d5c898ea4
Yasin Shuman <yshuman@loyalloops.com>
2017-07-18 13:39:13 -0700

    beginning a simple video app with a playlist and video player

--- app/ts/app.component.ts
+++ app/ts/app.component.ts
@@ -2,7 +2,9 @@ import {Component} from 'angular2/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>Angular 2 Template</h1>'
+    template: '<h1>{{mainHeading}}</h1>'
 })
 
-export class AppComponent {}
+export class AppComponent {
+    mainHeading = "My Videos";
+}
